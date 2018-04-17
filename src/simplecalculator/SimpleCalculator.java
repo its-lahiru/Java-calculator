4e7168a6d96e88f9bbf921845b630ad9c541ae35
Lahiru Sandaruwan <lahirus250@gmail.com>
2018-04-16 19:41:25 -0700

    first commit

---
+++ src/simplecalculator/SimpleCalculator.java
@@ -0,0 +1,11 @@
+package simplecalculator;
+
+public class SimpleCalculator {
+
+    
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        c.setVisible(true);
+    }
+    
+}
